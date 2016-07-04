af82d082ec21afae1a2c709a1b452e3bb869d9ea
sysatom <sysatom@gmail.com>
2016-07-04 22:34:51 +0800

    html

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -38,14 +38,14 @@
                 <div class="brand-wrapper">
                     <h2 class="section-title">@lang('front.topic')</h2>
                     <div class="brand-list">
-                        <img src="img/services_logo__1.jpg" alt="">
-                        <img src="img/services_logo__2.jpg" alt="">
-                        <img src="img/services_logo__3.jpg" alt="">
-                        <img src="img/services_logo__4.jpg" alt="">
-                        <img src="img/services_logo__1.jpg" alt="">
-                        <img src="img/services_logo__2.jpg" alt="">
-                        <img src="img/services_logo__3.jpg" alt="">
-                        <img src="img/services_logo__4.jpg" alt="">
+                        <img src="{{ url("img/services_logo__1.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__2.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__3.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__4.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__1.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__2.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__3.jpg") }}" alt="">
+                        <img src="{{ url("img/services_logo__4.jpg") }}" alt="">
                     </div>
                 </div>
             </div>
