5a8a5a536e4b44f74ef287c08b933cc75bf5aea0
sysatom <sysatom@gmail.com>
2016-07-06 14:19:16 +0800

    html

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -124,55 +124,55 @@
                         <li role="presentation" class="dropdown">
                             <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
                                 <i class="fa fa-envelope-o"></i>
-                                <span class="badge bg-green">6</span>
+                                {{--<span class="badge bg-green">6</span>--}}
                             </a>
                             <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
                                 <li>
                                     <a>
                                         <span class="image"><img src="/components/gentelella/production/images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                                        <span>
+                                          <span>John Smith</span>
+                                          <span class="time">3 mins ago</span>
+                                        </span>
+                                        <span class="message">
+                                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                                        </span>
                                     </a>
                                 </li>
                                 <li>
                                     <a>
                                         <span class="image"><img src="/components/gentelella/production/images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                                        <span>
+                                          <span>John Smith</span>
+                                          <span class="time">3 mins ago</span>
+                                        </span>
+                                        <span class="message">
+                                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                                        </span>
                                     </a>
                                 </li>
                                 <li>
                                     <a>
                                         <span class="image"><img src="/components/gentelella/production/images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                                        <span>
+                                          <span>John Smith</span>
+                                          <span class="time">3 mins ago</span>
+                                        </span>
+                                        <span class="message">
+                                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                                        </span>
                                     </a>
                                 </li>
                                 <li>
                                     <a>
                                         <span class="image"><img src="/components/gentelella/production/images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                                        <span>
+                                          <span>John Smith</span>
+                                          <span class="time">3 mins ago</span>
+                                        </span>
+                                        <span class="message">
+                                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                                        </span>
                                     </a>
                                 </li>
                                 <li>
