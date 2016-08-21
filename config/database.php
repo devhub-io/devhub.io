38d8b1d659bc1d07439daddd7108d7959bf5b4a3
sysatom <sysatom@gmail.com>
2016-08-21 18:30:26 +0800

    session redis

--- config/database.php
+++ config/database.php
@@ -115,6 +115,12 @@ return [
             'database' => 0,
         ],
 
+        'session' => [
+            'host' => env('REDIS_HOST', 'localhost'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
     ],
 
 ];
