2583cc4e988f4690378edded432ff719b4ca9e4d
sysatom <sysatom@gmail.com>
2016-08-31 11:24:30 +0800

    remove datatables

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,9 +19,6 @@
     <link href="/components/gentelella/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
     <!-- bootstrap-progressbar -->
     <link href="/components/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-    <!-- datatables -->
-    <link href="/components/datatables.net-dt/css/jquery.dataTables.min.css" rel="stylesheet">
-
     <!-- Custom Theme Style -->
     <link href="/components/gentelella/build/css/custom.min.css" rel="stylesheet">
 
@@ -246,10 +243,6 @@
 <script src="/components/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
 <!-- iCheck -->
 <script src="/components/gentelella/vendors/iCheck/icheck.min.js"></script>
-<!-- datatables -->
-<script src="/components/datatables.net/js/jquery.dataTables.min.js"></script>
-<!-- Custom Theme Scripts -->
-<script src="/components/gentelella/build/js/custom.min.js"></script>
 
 @yield('scripts')
 </body>
