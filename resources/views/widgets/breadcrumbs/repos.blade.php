79a77ab7196fae4bb9ab9082cc6ffc001cdbc0dc
sysatom <sysatom@gmail.com>
2017-02-03 16:31:28 +0800

    fix

--- resources/views/widgets/breadcrumbs/repos.blade.php
+++ resources/views/widgets/breadcrumbs/repos.blade.php
@@ -1,6 +1,6 @@
 <ol class="breadcrumb">
     <li><a href="{{ l_url('/') }}">Home</a></li>
     <li><a href="{{ l_url('list/popular') }}">Repositories</a></li>
-    <li><a href="{{ l_url('category', $repos->category->slug) }}">{{ trans("category.{$repos->category->slug}") }}</a></li>
+    <li><a href="{{ l_url('category', $repos->category ? $repos->category->slug : '') }}">{{ $repos->category ? trans("category.{$repos->category->slug}") : '' }}</a></li>
     <li class="active">{{ $repos->title }}</li>
 </ol>
--- resources/views/widgets/breadcrumbs/repos_news.blade.php
+++ resources/views/widgets/breadcrumbs/repos_news.blade.php
@@ -1,7 +1,7 @@
 <ol class="breadcrumb">
     <li><a href="{{ l_url('/') }}">Home</a></li>
     <li><a href="{{ l_url('list/popular') }}">Repositories</a></li>
-    <li><a href="{{ l_url('category', $repos->category->slug) }}">{{ trans("category.{$repos->category->slug}") }}</a></li>
+    <li><a href="{{ l_url('category', $repos->category ? $repos->category->slug : '') }}">{{ $repos->category ? trans("category.{$repos->category->slug}") : '' }}</a></li>
     <li><a href="{{ l_url('repos', $repos->slug) }}">{{ $repos->title }}</a></li>
     <li class="active">News</li>
 </ol>
