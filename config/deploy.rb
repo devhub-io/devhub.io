76a30fb663f7fb874f7d03c6dc3673bda414d4e7
sysatom <sysatom@gmail.com>
2017-03-13 17:24:02 +0800

    deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,59 @@
+# config valid only for current version of Capistrano
+lock "3.8.0"
+
+set :application, "develophub"
+set :repo_url, "git@bitbucket.org:sysatom/develophub.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/www/devhub"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+set :format_options, command_output: true, log_file: "storage/logs/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+##### Laravel
+
+# The version of laravel being deployed
+set :laravel_version, 5.4
+
+# Linked directories for a standard Laravel 5 application
+set :laravel_5_linked_dirs, [
+  'storage'
+]
+
+# Set ACLs for the paths in laravel_acl_paths?
+set :laravel_set_acl_paths, true
+
+# Paths that should have ACLs set for a standard Laravel 5 application
+set :laravel_5_acl_paths, [
+  'bootstrap/cache',
+  'storage',
+  'storage/app',
+  'storage/app/public',
+  'storage/framework',
+  'storage/framework/cache',
+  'storage/framework/sessions',
+  'storage/framework/views',
+  'storage/logs'
+]
