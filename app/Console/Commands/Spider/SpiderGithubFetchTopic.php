06c807d433bf73cd12dbd691a2f958b7d4145d6e
sysatom <sysatom@gmail.com>
2017-03-17 15:22:29 +0800

    topics

--- app/Console/Commands/Spider/SpiderGithubFetchTopic.php
+++ app/Console/Commands/Spider/SpiderGithubFetchTopic.php
@@ -12,8 +12,6 @@
 namespace App\Console\Commands\Spider;
 
 use Illuminate\Console\Command;
-use Symfony\Component\CssSelector\CssSelectorConverter;
-use Symfony\Component\DomCrawler\Crawler;
 
 class SpiderGithubFetchTopic extends Command
 {
@@ -44,38 +42,62 @@ class SpiderGithubFetchTopic extends Command
      */
     public function handle()
     {
-        @unlink(storage_path('topic.txt'));
-
-        $url = "https://github.com/search?q=topic%3Anodejs&type=Repositories&p=";
-
-
-
+        $handle = fopen(storage_path('topics-' . date('Y-m-d') . '.txt'), 'a+');
+        $topic_keyword = 'nodejs';
+        $url = "https://github.com/search?o=desc&q=topic%3A$topic_keyword&s=stars&type=Repositories&p=";
         foreach (range(1, 100) as $page) {
-            $html = @file_get_contents($url . $page);
+            $html = file_get_contents($url . $page);
+            $li = explode('<li class="col-12 d-block width-full py-4 border-bottom public source">', $html);
+            foreach ($li as $item) {
+                $repos_re = '/<a href="(.*)" class="v-align-middle">(.*)<\/a>/';
+                preg_match($repos_re, $item, $repos_matches);
 
+                if ($repos_matches && isset($repos_matches[1])) {
+                    $name = trim($repos_matches[2]);
+                    $repos_url = "https://github.com" . trim($repos_matches[1]);
 
-            $crawler = new Crawler($html);
-            $filter = $crawler->filter('li');
-            foreach ($filter as $item) {
-                $crawler = new Crawler($item);
-                echo $crawler->filter('p')->text();
-                echo "\n";
-           }
+                    $topic_re = '/<a href=".*" class=".*" data-ga-click=".*".*>([\w\W\s\n]*?)<\/a>/';
+                    preg_match_all($topic_re, $item, $topic_matches, PREG_SET_ORDER, 0);
+                    $topics = [];
+                    foreach ($topic_matches as $tm_item) {
+                        $topics[] = isset($tm_item[1]) ? trim($tm_item[1]) : '';
+                    }
 
-dd(22);
-            $github_urls = [];
-            if (isset($matches[1])) {
-                foreach ($matches[1] as $item) {
-                    $github_urls[] = 'https://github.com' . $item;
+                    $desc_re = '/<p class="col-9 text-gray pr-4 py-1 mb-2">([\w\W\s\n]*?)<\/p>/';
+                    preg_match($desc_re, $item, $desc_matches);
+                    $desc = isset($desc_matches[1]) ? $desc_matches[1] : '';
+                    $desc = trim(strip_tags($desc));
+
+                    $lang_re = '/<span class="mr-3">(.*)<\/span>/';
+                    preg_match($lang_re, $item, $lang_matches);
+                    $lang = isset($lang_matches[1]) ? trim($lang_matches[1]) : '';
+
+                    $stargazers_re = '/<a class="muted-link tooltipped tooltipped-s mr-3" href=".*" aria-label="Stargazers">[\w\W\s\n]*?<\/svg>([\w\W\s\n]*?)<\/a>/';
+                    preg_match($stargazers_re, $item, $stargazers_matches);
+                    $stargazers = isset($stargazers_matches[1]) ? trim($stargazers_matches[1]) : 0;
+                    $stargazers = str_replace(',', '', $stargazers);
+
+                    $forks_re = '/<a class="muted-link tooltipped tooltipped-s mr-3" href=".*" aria-label="Forks">[\w\W\s\n]*?<\/svg>([\w\W\s\n]*?)<\/a>/';
+                    preg_match($forks_re, $item, $forks_matches);
+                    $forks = isset($forks_matches[1]) ? trim($forks_matches[1]) : 0;
+                    $forks = str_replace(',', '', $forks);
+
+                    $updated_re = '/<relative-time datetime="(.*)">.*<\/relative-time>/';
+                    preg_match($updated_re, $item, $updated_matches);
+                    $updated = isset($updated_matches[1]) ? trim($updated_matches[1]) : 0;
+                    $updated = str_replace('T', ' ', $updated);
+                    $updated = str_replace('Z', '', $updated);
+
+                    $repos = ['name' => $name, 'url' => $repos_url, 'desc' => $desc, 'topics' => $topics, 'lang' => $lang,
+                        'stargazers' => $stargazers, 'forks' => $forks, 'updated' => $updated];
+
+                    fwrite($handle, "\n" . json_encode($repos));
+                    echo "\n" . json_encode($repos) . "\n";
                 }
             }
-            $text = implode("\n", $github_urls);
-            $handle = fopen(storage_path('topic.txt'), 'a+');
-            fwrite($handle, "\n" . $text);
 
             $this->info("Page: $page");
             sleep(10);
-            exit;
         }
 
         $this->info('All Done');
